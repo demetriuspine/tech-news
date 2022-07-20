39fa08340ef4d4d5cdb5443f9ee63ffdecc66e94
lucas <mr.pine97@gmail.com>
2022-07-20 16:41:11 -0300

    req01 - feat: implementa a função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import time
+import requests
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        headers = {"user-agent": "Fake user-agent"}
+        response = requests.get(url, headers=headers, timeout=3)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
