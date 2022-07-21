c836e1400b3e1c53155b3c6aff875551dfb5fa5d
lucas <mr.pine97@gmail.com>
2022-07-21 11:26:03 -0300

    req06 - feat: implementa a função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,19 @@
 # Requisito 6
+from tech_news.database import search_news
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    news_list = search_news(query={
+            'title': {'$regex': title, "$options": "i"}
+        })
+    news_tuples_list = []
+
+    for news in news_list:
+        news_tuple = tuple([news['title'], news['url']])
+        news_tuples_list.append(news_tuple)
+
+    return news_tuples_list
 
 
 # Requisito 7
